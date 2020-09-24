43fa9a7e36d79a321cf7b5731499b8eee4b9dcdb
MMAcode <miroslav.makarov@gmail.com>
2020-09-24 14:29:28 +0100

    Pawn stays on the board

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -15,21 +15,18 @@ public class Pawn extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        //list
         List <Move> allowedMoves = new ArrayList<>();
-
 
         boolean black = this.getColour().equals(PlayerColour.BLACK);
         int moveBy = black ? 1 : -1;
         Coordinates to = new Coordinates(from.getRow()+moveBy, from.getCol());
-        boolean squareAvailable = (board.get(to)==null);
-        if (squareAvailable) allowedMoves.add(new Move(from,to));
+        boolean squareExists = (to.getRow()>=0 && to.getCol() >= 0)&&(to.getRow()<=7 && to.getCol() <= 7);
+        boolean squareAvailable = squareExists ? (board.get(to)==null) : false;
+        if (squareExists && squareAvailable) allowedMoves.add(new Move(from,to));
 
-
-
-        //implement first move
-        boolean firstMove = ((black && from.getRow()==1)||(!black && from.getRow()==6))? true : false;
-        if (firstMove){
+        //implement first (="double") move
+        boolean isFirstMove = ((black && from.getRow()==1)||(!black && from.getRow()==6))? true : false;
+        if (isFirstMove){
             Coordinates to2 = new Coordinates(from.getRow()+moveBy*2, from.getCol());
             squareAvailable = (board.get(to2)==null);
             if (squareAvailable && allowedMoves.size()>0) allowedMoves.add(new Move(from,to2));
