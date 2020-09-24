3e2733637289a495bd105b68c3f18c135a98f8ab
MMAcode <miroslav.makarov@gmail.com>
2020-09-24 14:09:05 +0100

    Pawn recognises pieces ahead

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -15,21 +15,24 @@ public class Pawn extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
+        //list
+        List <Move> allowedMoves = new ArrayList<>();
+
+
         boolean black = this.getColour().equals(PlayerColour.BLACK);
         int moveBy = black ? 1 : -1;
         Coordinates to = new Coordinates(from.getRow()+moveBy, from.getCol());
+        boolean squareAvailable = (board.get(to)==null);
+        if (squareAvailable) allowedMoves.add(new Move(from,to));
 
-        Move allowedMove = new Move(from,to);
-
-        //list
-        List <Move> allowedMoves = new ArrayList<>();
-        allowedMoves.add(allowedMove);
 
 
         //implement first move
         boolean firstMove = ((black && from.getRow()==1)||(!black && from.getRow()==6))? true : false;
         if (firstMove){
-            allowedMoves.add(new Move(from, new Coordinates(from.getRow()+moveBy*2, from.getCol())));
+            Coordinates to2 = new Coordinates(from.getRow()+moveBy*2, from.getCol());
+            squareAvailable = (board.get(to2)==null);
+            if (squareAvailable && allowedMoves.size()>0) allowedMoves.add(new Move(from,to2));
         }
 
         return allowedMoves;
